5f1509874154776ac70a24edc4630764d916c169
Eszter Mázi <mazieszter@gmail.com>
2025-12-10 14:40:23 +0100

    Add bookings route

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { auth } from "./middleware/auth.js";
+// import { auth } from "./middleware/auth.js";
+import bookingsRoute from "./routes/bookings.js";
 
 const app = express();
 app.use(express.json());
-app.use(auth);
+// app.use(auth);    // Re-enable later
+
+app.use("/bookings", bookingsRoute);
 
 app.get('/', (req, res) => {
     res.json({ message: "BBB Backend API is running"});
