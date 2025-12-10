08bb360642ee2ff409fa8542235871e9ad3a20c9
Eszter Mázi <mazieszter@gmail.com>
2025-12-10 16:06:14 +0100

    Add GET /bookings endpoint with email filtering

--- src/routes/bookings.js
+++ src/routes/bookings.js
@@ -2,11 +2,18 @@ import express from "express";
 import { readJSON } from "../utils/storage.js";
 const router = express.Router()
 
-const FILE_PATH = "./src/data/bookings.json";
+const BOOKINGS_PATH = "./src/data/bookings.json";
 
 router.get("/", (req, res) => {
-    const bookings = readJSON(FILE_PATH);
-    res.json(bookings);
+  const email = req.query.email;
+  const bookings = readJSON(BOOKINGS_PATH);
+  let result = bookings;
+
+  if (email) {
+    result = bookings.filter(b => b.email === email);
+  }
+
+  return res.json(result);
 });
 
 export default router;
